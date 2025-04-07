7024f0147e9cfde12d26040b9b02f67921e93091
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2025-04-07 16:21:59 +0300

    config

---
+++ src/Config/ConfigRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace IncidentCenter\RL\CloudFunctions\ParserOpenRfmFullList\Config;
+
+class ConfigRepository
+{
+    private static array $config = [];
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public static function get($key): mixed
+    {
+        if (!self::$config) {
+            self::$config = require_once __DIR__ . 'config.php';
+        }
+
+        return self::$config[$key];
+    }
+}
